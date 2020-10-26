fca1dde736427c9f6823715ff373da7fe4d91364
Eric Walter <ericwalter@my.cwi.edu>
2020-10-26 13:25:27 -0600

    First Commit

---
+++ RailShooter/Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    //[SerializeField] GameObject breakFX;
+    void Start()
+    {
+
+    }
+
+
+    void OnMouseDown()
+    {
+        // Destroys target when clicked on
+        Destroy(gameObject);
+
+        // Activates break particles
+        //breakFX.SetActive(true);
+    }
+}
