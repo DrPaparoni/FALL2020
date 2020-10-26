2531f16a7826ec29ba3749e151817c169eaf5277
Eric Walter <ericwalter@my.cwi.edu>
2020-10-26 14:11:03 -0600

    Fixed issue that broke particle and sound fx

--- RailShooter/Assets/Scripts/Target.cs
+++ RailShooter/Assets/Scripts/Target.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    //[SerializeField] GameObject breakFX;
+    [SerializeField] GameObject breakFX;
+    [SerializeField] Transform parent; // placeholder to destroy deathFX when done
+    [SerializeField] int scorePerHit = 12;
+
+    ScoreBoard scoreBoard;
     void Start()
     {
-
+        scoreBoard = FindObjectOfType<ScoreBoard>();
     }
 
 
     void OnMouseDown()
     {
-        // Destroys target when clicked on
+        GameObject fx = Instantiate(breakFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        //ScoreBoard scoreBoard = FindObjectOfType<ScoreBoard>();
+        scoreBoard.ScoreHit(scorePerHit);
+
         Destroy(gameObject);
 
-        // Activates break particles
-        //breakFX.SetActive(true);
     }
 }
