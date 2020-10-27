d237c8ff1b053f603d3c2fbf2f73987287ec3376
Eric Walter <ericwalter@my.cwi.edu>
2020-10-27 08:59:50 -0600

    Imported house model, added tag counting system

--- RailShooter/Assets/Scripts/Target.cs
+++ RailShooter/Assets/Scripts/Target.cs
@@ -2,19 +2,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Target : MonoBehaviour
 {
     [SerializeField] GameObject breakFX;
     [SerializeField] Transform parent; // placeholder to destroy deathFX when done
     [SerializeField] int scorePerHit = 12;
-
+    
+    GameObject[] targets;
     ScoreBoard scoreBoard;
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>();
+        
     }
 
+    void Update()
+    {
+        
+    }
 
     void OnMouseDown()
     {
@@ -22,8 +29,27 @@ public class Target : MonoBehaviour
         fx.transform.parent = parent;
         //ScoreBoard scoreBoard = FindObjectOfType<ScoreBoard>();
         scoreBoard.ScoreHit(scorePerHit);
+        findTargets();
 
         Destroy(gameObject);
+        print("Targets left: " + targets.Length);
+        if (targets.Length == 1)
+        {
+            LoadNextLevel();
+        }
+    }
 
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    private void findTargets()
+    {
+        targets = GameObject.FindGameObjectsWithTag("Target");
+        int numOfTargets = targets.Length;
     }
 }
